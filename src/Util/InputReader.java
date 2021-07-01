82be5790ed0ffde3280a3d36200a41651569256f
furkansoyturk <24878062+furkansoyturk@users.noreply.github.com>
2021-07-01 04:38:05 +0300

    Update InputReader
    jswing added.

--- src/Util/InputReader.java
+++ src/Util/InputReader.java
@@ -1,25 +1,34 @@
 package Util;
 
+import javax.swing.*;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 public class InputReader {
-
-    public static String readFileAsString(String fileName)throws Exception
+    
+    public static String readFileAsString(File fileName)throws Exception
     {
         String data = "";
-        data = new String(Files.readAllBytes(Paths.get(fileName)));
-        System.out.println("Verilen input adı: " +Paths.get(fileName));
+        data = new String(Files.readAllBytes(Paths.get(String.valueOf(fileName))));
+        System.out.println("Seçilen input dosyası: " +Paths.get(String.valueOf(fileName)));
         return data;
     }
 
-    public List<String> reader() throws Exception {
-        String inputTxt = readFileAsString("src/input/board2.txt");
+    public static List<String> reader() throws Exception {
+
+        JFileChooser f = new JFileChooser();
+        f.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        f.showSaveDialog(null);
+
+        
+        String inputTxt = readFileAsString(f.getSelectedFile());
         inputTxt = inputTxt.replaceAll("\\s+","");
 
         List<String> currentBoard = Arrays.asList(inputTxt.split("(?<=\\G.{2})"));
+        
         return  currentBoard;
     }
 
